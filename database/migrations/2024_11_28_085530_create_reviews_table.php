ac0e837c49c780d1ce21206d56e1b80a8fe592c5
ezu0 <burimofumofu@gmail.com>
2024-11-29 08:07:17 +0900

    1対多のマイグレーション Book - Review

--- database/migrations/2024_11_28_085530_create_reviews_table.php
+++ database/migrations/2024_11_28_085530_create_reviews_table.php
@@ -13,9 +13,15 @@ return new class extends Migration
   {
     Schema::create('reviews', function (Blueprint $table) {
       $table->id();
+      // MEMO: Bookテーブルと紐づけるためのID？
       $table->text('review');
       $table->unsignedTinyInteger('rating');
       $table->timestamps();
+      // $table->unsignedBigInteger('book_id');
+      // $table->foreign('book_id')->references('id')->on('books')
+      //   ->onDelete('cascade');
+      //MEMO: ↑の2行でも同じ意味の実装ができる
+      $table->foreignId('book_id')->constrained()->cascadeOnDelete();
     });
   }
 
