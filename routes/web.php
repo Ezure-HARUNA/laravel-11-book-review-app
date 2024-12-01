7a3ebe38656c6041f43cfdd092003d87228f7f06
ezu0 <burimofumofu@gmail.com>
2024-12-01 16:59:33 +0900

    リソースコントローラとControllerの追加、

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
   return view('index');
 });
+
+//MEMO: この1行を書くだけで、
+//      booksページのCRUD処理の各ページのルート設定ができる。
+Route::resource('books', BookController::class);
