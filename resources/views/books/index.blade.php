70ca9f8845cb35e2d73959e196f93dd29ccbba6c
ezu0 <burimofumofu@gmail.com>
2024-12-01 18:52:30 +0900

    feat: 検索

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -2,9 +2,13 @@
 
 @section('content')
   <h1 class="mb-10 text-2xl">書籍</h1>
-  <form action="">
-
+  <form action="{{ route('books.index') }}" method="GET" class="mb-4 flex gap-2">
+    <input type="text" name="title" placeholder="タイトルを検索してください" value="{{ request('title') }}"
+      class="w-full md:w-1/2 mb-2 px-3 py-2 border rounded h-10">
+    <button type="submit" class="btn h-10"><i class="fas fa-search"></i> 検索</button>
+    <a href="{{ route('books.index') }}" class="btn h-10">クリア</a>
   </form>
+
   <ul>
     @forelse ($books as $book)
       <li class="mb-4">
@@ -15,7 +19,7 @@
               <span class="book-author">{{ $book->author }}</span>
             </div>
             <div>
-              <div class="book-rating mb-2"> <i class="fas fa-star"></i>{{ number_format($book->reviews_avg_rating, 1) }}
+              <div class="book-rating mb-2"><i class="fas fa-star"></i>{{ number_format($book->reviews_avg_rating, 1) }}
               </div>
               <div class="book-review-count">
                 {{ $book->reviews_count }} {{ Str::plural('review', $book->reviews_count) }}レビュー
